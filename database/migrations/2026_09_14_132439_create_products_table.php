8bbdba987c91beb377245efae6c6ee5c9957978e
ranith12 <ranith180@gmail.com>
2026-09-15 19:34:51 +0700

    create products migration

--- database/migrations/2026_09_14_132439_create_products_table.php
+++ database/migrations/2026_09_14_132439_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->decimal('price', 10, 2);
+            $table->unsignedInteger('qty')->default(0);
+            $table->decimal('discount', 5, 2)->default(0);
+            $table->text('des')->nullable();
             $table->timestamps();
         });
     }
